2b8387ea2b16589f5a0c20e6b234cbc216eeb1b2
chucklebiteanimations-rgb <chucklebiteanimations@gmail.com>
2026-01-27 12:24:06 +0530

    Update keep_alive.py

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -7,8 +7,11 @@ app = Flask('')
 def home():
     return "I am alive! Bot is running."
 
+import os
+
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def start():
     t = Thread(target=run)
