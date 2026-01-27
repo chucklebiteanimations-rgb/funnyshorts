dba59503e56bc56d24c15bb7ab9d659637b7f74c
chucklebiteanimations-rgb <chucklebiteanimations@gmail.com>
2026-01-27 12:48:08 +0530

    Update keep_alive.py

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -13,6 +13,7 @@ def run():
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
-def start():
-    t = Thread(target=run)
-    t.start()
+
+# def start():
+#     t = Thread(target=run)
+#     t.start()
